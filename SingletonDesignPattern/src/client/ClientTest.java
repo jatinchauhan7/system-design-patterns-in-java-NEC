c21ea9e9faeca430480acba197e359408291b323
jatinchauhan7 <106475386+jatinchauhan7@users.noreply.github.com>
2023-11-23 12:25:28 +0530

    Add files via upload

---
+++ SingletonDesignPattern/src/client/ClientTest.java
@@ -0,0 +1,18 @@
+package client;
+
+import singleton.Singleton;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		
+		System.out.println(singleton1.hashCode());
+		System.out.println(singleton2.hashCode());
+		
+	}
+
+}
---
+++ SingletonDesignPattern/src/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package singleton;
+
+
+public class Singleton {
+
+	private static Singleton INSTANCE = null;
+
+	
+	private Singleton() {
+
+	}
+
+	
+	public static Singleton getInstance() {
+		
+		if (INSTANCE == null) {
+			INSTANCE = new Singleton();
+		}
+		return INSTANCE;
+
+	}
+}
